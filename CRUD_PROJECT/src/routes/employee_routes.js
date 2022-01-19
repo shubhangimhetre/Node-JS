8f9126e44f0bded6c6033b03caa3096f662c38e5
Shubhangi Vishwanath Mhetre <89292248+shubhangivm@users.noreply.github.com>
2022-01-19 12:45:48 +0530

    Add files via upload

--- CRUD_PROJECT/src/routes/employee_routes.js
+++ CRUD_PROJECT/src/routes/employee_routes.js
@@ -1,1 +1,23 @@
+const express = require('express')
+const router = express.Router()
+const employeeController =   require('../controllers/employee_controllers');
 
+// Retrieve all employees
+router.get('/', employeeController.findAll);
+
+// Create a new employee
+router.post('/', employeeController.create);
+
+// Retrieve a single employee with id
+router.get('/:id', employeeController.findById);
+
+// // Update a employee with id
+// router.patch('/:id', employeeController.update);
+
+// Update a employee with id
+router.put('/:id', employeeController.update);
+
+// Delete a employee with id
+router.delete('/:id', employeeController.delete);
+
+module.exports = router
